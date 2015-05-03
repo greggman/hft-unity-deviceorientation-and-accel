f651f849f4363281750b7e9f40b55a7ff63c75c3
Gregg Tavares <github@greggman.com>
2015-05-03 21:44:57 +0900

    add names

--- Assets/HappyFunTimes/Scripts/PlayerScript.cs
+++ Assets/HappyFunTimes/Scripts/PlayerScript.cs
@@ -86,10 +86,32 @@ class PlayerScript : MonoBehaviour {
     private void SetColor(Color color) {
         m_color = color;
         m_renderer.material.color = m_color;
+        Color[] pix = new Color[1];
+        pix[0] = color;
+        Texture2D tex = new Texture2D(1, 1);
+        tex.SetPixels(pix);
+        tex.Apply();
+        m_guiStyle.normal.background = tex;
     }
 
-    private void SetName(string name) {
+    void OnGUI()
+    {
+        Vector2 size = m_guiStyle.CalcSize(m_guiName);
+        Vector3 coords = Camera.main.WorldToScreenPoint(transform.position);
+        m_nameRect.x = coords.x - size.x * 0.5f - 5.0f;
+        m_nameRect.y = Screen.height - coords.y - 40.0f;
+        GUI.Box(m_nameRect, m_name, m_guiStyle);
+    }
+
+    void SetName(string name) {
         m_name = name;
+        gameObject.name = "Player-" + m_name;
+        m_guiName = new GUIContent(m_name);
+        m_guiStyle.normal.textColor = Color.black;
+        m_guiStyle.contentOffset = new Vector2(4.0f, 2.0f);
+        Vector2 size = m_guiStyle.CalcSize(m_guiName);
+        m_nameRect.width  = size.x + 12;
+        m_nameRect.height = size.y + 5;
     }
 
     public void OnTriggerEnter(Collider other) {
@@ -137,6 +159,9 @@ class PlayerScript : MonoBehaviour {
     private Vector3 m_position;
     private Color m_color;
     private string m_name;
+    private GUIStyle m_guiStyle = new GUIStyle();
+    private GUIContent m_guiName = new GUIContent("");
+    private Rect m_nameRect = new Rect(0,0,0,0);
 }
 
 
